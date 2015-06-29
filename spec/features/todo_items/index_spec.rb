c4808d32cf2a86a4a2b9a891437c9f0c921b9d0e
Sage Elliott <wildtroll@gmail.com>
2015-06-29 15:23:41 -0700

    added the following - bcrypt gem - rspec fixes by updating expecting unordered lists to tables. in todo itemsspecs.

--- spec/features/todo_items/index_spec.rb
+++ spec/features/todo_items/index_spec.rb
@@ -6,14 +6,14 @@ describe "viewing todo items" do
 
 	it "displays the title of the todo list" do
 		visit_todo_list(todo_list)
-		within("h1") do
+		within("h2") do
 			expect(page).to have_content(todo_list.title)
 		end
 	end
 
 	it "displays no items when a todo list is empty" do
 		visit_todo_list(todo_list)
-		expect(page.all("ul.todo_items li").size).to eq(0)
+		expect(page.all("table.todo_items td").size).to eq(0)
 	end
 
 	it "displays item content when a todo list has item" do
@@ -22,9 +22,9 @@ describe "viewing todo items" do
 
 		visit_todo_list(todo_list)
 
-		expect(page.all("ul.todo_items li").size).to eq(2)
+		expect(page.all("table.todo_items tr").size).to eq(3)
 
-		within "ul.todo_items" do
+		within "table.todo_items" do
 			expect(page).to have_content("Milk")
 			expect(page).to have_content("Eggs")
 		end
